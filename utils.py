42b458994385eb852b731ba96bd1db9dd39d7449
pauloakira <paulo@enabe.com.br>
2023-12-31 18:07:13 -0300

    Feature engineering function to add numerical info

--- utils.py
+++ utils.py
@@ -1,8 +1,12 @@
 import re
+import pandas as pd
+
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer, PorterStemmer
+
+from textblob import TextBlob
 
 def donwload_nltk_resources(resource_name: str):
     try:
@@ -43,3 +47,27 @@ def cleanText(text: str, withStemming = False)-> str:
     # Join words
     text = ' '.join(words)
     return text
+
+def computeNumericalFeatures(df: pd.DataFrame):
+    ''' Receives a dataframe with a column named 'text' (already cleaned) 
+    and computes the following features: number of words, number of characters,
+    average word length, number of sentences and sentiment score.
+
+    Input:
+        - df: dataframe with a column named 'text' (already cleaned).
+    
+    Output:
+        - df: dataframe with the new features.
+    '''
+    # Number of words
+    df['num_words'] = df['text'].apply(lambda x: len(str(x).split()))
+    # Character count
+    df['num_chars'] = df['text'].apply(lambda x: len(str(x)))
+    # Average word length
+    df['avg_word_length'] = df['num_chars'] / df['num_words']
+    # Sentence count
+    df['num_sentences'] = df['text'].apply(lambda x: len(TextBlob(x).sentences))
+    # Sentiment score
+    df['sentiment_score'] = df['text'].apply(lambda x: TextBlob(x).sentiment.polarity)
+
+    return df
